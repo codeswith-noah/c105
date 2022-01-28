1280ca84e9990ec54621763d1357521aeea7cd25
codeswith-noah <85592454+codeswith-noah@users.noreply.github.com>
2022-01-28 18:26:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Webcam.set({
+    width:360,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '">';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/O0yhLwWaR/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log("model load succesfully");
+}
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, getResult);
+}
+
+function getResult(error, result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+        document.getElementById("result_object_name").innerHTML = result[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(3);
+    }
+}
